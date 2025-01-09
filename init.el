02e76c9e4f1974c34dc62a58b5aa636f23c93942
Krishna Subramaniyan <krishnasubramaniyan2001@gmail.com>
2025-01-09 21:58:12 +0530

    removed init2 updated init.el

--- init.el
+++ init.el
@@ -2,19 +2,3 @@
  (expand-file-name 
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("95e934b092694a2624adb653043d1dc016a6712fa27b788f9ff4dffb8ee08290" "515ebca406da3e759f073bf2e4c8a88f8e8979ad0fdaba65ebde2edafc3f928c" default))
- '(ispell-dictionary nil)
- '(package-selected-packages
-   '(emmet-mode doom-modeline nerd-icons ef-themes marginalia use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
